81cc22c87a5ed529c13fb87dcb22f70d960f08bf
lihuelmujica <lihuelmujica@gmail.com>
2024-08-27 23:50:49 -0300

    fix issuer property and user role este sí final

--- msvc-usuarios/src/main/java/com/espe/msvc/usuarios/msvc_usuarios/security/WebSecurityConfig.java
+++ msvc-usuarios/src/main/java/com/espe/msvc/usuarios/msvc_usuarios/security/WebSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    public static final String USER = "user";
+    public static final String USER = "usuario";
     public static final String ADMIN ="admin";
     private final JwtAuthConverter jwtAuthConverter;
 
